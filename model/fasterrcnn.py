4c013b6510a0a04fc5aca16cfbaa5ccb99dfb5dc
nizar-masmoudi <nizarmasmoudi@outlook.fr>
2023-01-25 18:00:40 +0100

    Fixed model forward method

--- model/fasterrcnn.py
+++ model/fasterrcnn.py
@@ -2,13 +2,14 @@ import torch
 import torch.nn as nn
 from torchvision.models.detection import fasterrcnn_resnet50_fpn
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from typing import Any
 
 class FasterRCNN(nn.Module):
-  def __init__(self, num_classes: int, pretrained: bool = False) -> None:
-    super(FasterRCNN).__init__()
-    self.fasterrcnn = fasterrcnn_resnet50_fpn(pretrained = pretrained)
+  def __init__(self, num_classes: int, weights: Any = None) -> None:
+    super(FasterRCNN, self).__init__()
+    self.fasterrcnn = fasterrcnn_resnet50_fpn(weights = weights)
     in_features = self.fasterrcnn.roi_heads.box_predictor.cls_score.in_features
     self.fasterrcnn.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
     
-  def forward(self, x: torch.Tensor):
-    return self.fasterrcnn(x)
+  def forward(self, images: torch.Tensor, targets: torch.Tensor):
+    return self.fasterrcnn(images, targets)
--- test.py
+++ test.py
@@ -1,5 +1,19 @@
 from dataset.facemask import FaceMask
 from dataset.utils.transforms import ParseXML
 from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
+from model.fasterrcnn import FasterRCNN
 
 dataset = FaceMask(transform = ToTensor(), target_transform = ParseXML())
+
+# dataset.display(0)
+
+data_loader = DataLoader(dataset, batch_size = 1, shuffle = False, collate_fn = lambda batch: tuple(zip(*batch))) # Add custom collate_fn because DataLoader expects all inputs to be same size
+
+batch = next(iter(data_loader))
+img, target = batch
+print('Successfully loaded a batch of {} images and {} boxes'.format(len(img), len(target)))
+
+model = FasterRCNN(num_classes = 3)
+out = model(*batch)
+print('Output is', out)
