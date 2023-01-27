96095f68548d454a2f8cef83b59d73892c9774c4
nizar-masmoudi <nizarmasmoudi@outlook.fr>
2023-01-27 11:39:14 +0100

    Add model to device

--- train.py
+++ train.py
@@ -41,7 +41,7 @@ def main():
     device
   )
   
-  model = FasterRCNN(num_classes = 3)
+  model = FasterRCNN(num_classes = 3).to(device)
   opt = torch.optim.Adam(model.parameters(), lr = lr, weight_decay = weight_decay)
   
   model.fit(train_dl, valid_dl, metric = MeanAveragePrecision(), opt = opt, n_epochs = n_epochs)
